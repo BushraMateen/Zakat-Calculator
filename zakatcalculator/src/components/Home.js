c40682edda7ed6ac88051257ae51b78165bb0840
mastanpatel <Bushra.mateen95@outlook.com>
2022-04-25 12:43:51 +0530

    Components

---
+++ zakatcalculator/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Country from './Country'
+import Currency from './Currency'
+import Language from './Language'
+
+export default function Home() {
+  return (
+      <div>
+        <Country />
+        <Currency />
+        <Language />
+      </div>
+    
+
+
+  )
+}
