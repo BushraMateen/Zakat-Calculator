8b09cc9abcc4b6de94cc1002484cdc4918e6e571
mastanpatel <Bushra.mateen95@outlook.com>
2022-04-27 13:24:26 +0530

    Routes

--- zakatcalculator/src/App.js
+++ zakatcalculator/src/App.js
@@ -1,20 +1,27 @@
 
 import './App.css';
 import Home from './components/Home';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import ZakatTable from './Features/zakatForm/ZakatTable';
 
 function App() {
   return (
-    <div className="container dark">
+    <Router>
+      <div className="container dark">
         <div className="app">
         <div className='app-header'>
-        <h1>Zakat Calculator</h1></div>
-        <Home />
-        <div >
-        <button className='submit-btn'>Submit</button>
-      </div>
+        <h1>Zakat Calculator</h1>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/ZakatTable" element={<ZakatTable />}></Route>
+        </Routes>
+        
         </div>
        
-    </div>
+    </div>  
+    </Router>
+   
   );
 }
 
